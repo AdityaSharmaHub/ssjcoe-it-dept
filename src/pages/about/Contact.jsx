73bb6c42f7d8ffb9e56dc09213cace0b1738eb2d
Aditya Sharma <adisharma4857@gmail.com>
2023-10-13 09:40:22 +0530

    Enhanced layout

--- src/pages/about/Contact.jsx
+++ src/pages/about/Contact.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const ContactUs = () => {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-            <div className="p-4 bg-white rounded shadow-xl">
-                <h2 className="text-2xl font-bold mb-4">Contact Us</h2>
+        <div className="flex flex-col items-center justify-center bg-gray-100">
+            <div className="p-4 bg-white rounded shadow-xl my-10">
+                <h2 className="text-2xl font-bold mb-6">Contact Us</h2>
 
                 <div className="mb-4">
                     <h3 className="font-semibold">Mobile Number:</h3>
@@ -18,9 +18,8 @@ const ContactUs = () => {
 
                 <div className="mb-4">
                     <h3 className="font-semibold">Location:</h3>
-                    <div className="w-full h-64">
-                        {/* Replace the iframe src with your Google Maps embed link */}
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3505.328411490491!2d77.21672131507853!3d28.613939082422132!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cfd5b347eb62d%3A0x52c2b7494e204dce!2sIndia%20Gate!5e0!3m2!1sen!2sin!4v1630493124388!5m2!1sen!2sin" width="600" height="450" style={{border:0}} allowFullScreen="" loading="lazy"></iframe>
+                    <div className="w-full min-h-auto">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3767.9370329197936!2d73.10570917520839!3d19.197952382031914!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be795757569a46b%3A0x864b94d61f571b24!2sShivajirao%20S.%20Jondhale%20College%20of%20Engineering!5e0!3m2!1sen!2sin!4v1697169250945!5m2!1sen!2sin" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                     </div>
                 </div>
             </div>
--- src/pages/about/Developers.jsx
+++ src/pages/about/Developers.jsx
@@ -11,19 +11,19 @@ const Developers = () => {
           <div className='flex justify-around mb-20'>
 
             <div className='text-center font-semibold flex flex-col justify-center items-center'>
-              <img src={adityaphoto} className='mb-5' alt="Developer" width={300} />
+              <img src={adityaphoto} className='mb-5 shadow-md' alt="Developer" width={250} />
               <p className='text-lg'>Mr. Aditya Sharma</p>
               <p className='text-lg'>Full Stack Web Developer</p>
             </div>
           
             <div className='text-center font-semibold flex flex-col justify-center items-center'>
-              <img src={vishnuphoto} className='mb-5' alt="Developer" width={300} />
+              <img src={vishnuphoto} className='mb-5 shadow-md' alt="Developer" width={250} />
               <p className='text-lg'>Mr. Vishnu Yelde</p>
               <p className='text-lg'>Frontend Developer</p>
             </div>
           
             <div className='text-center font-semibold flex flex-col justify-center items-center'>
-              <img src={prachitiphoto} className='mb-5' alt="Developer" width={300} />
+              <img src={prachitiphoto} className='mb-5 shadow-md' alt="Developer" width={250} />
               <p className='text-lg'>Miss Prachiti Yadav</p>
               <p className='text-lg'>Assistant Engineer</p>
             </div>
