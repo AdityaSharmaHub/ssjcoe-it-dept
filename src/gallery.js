dd9132259b7a8059ea660473acec284e7356cd0d
Aditya Sharma <adisharma4857@gmail.com>
2023-10-24 15:33:29 +0530

    added photos

---
+++ src/gallery.js
@@ -0,0 +1,53 @@
+import img1 from './assets/gallery/img1.jpg';
+// import img2 from './assets/gallery/img2.jpg';
+import img3 from './assets/gallery/img3.jpg';
+import img4 from './assets/gallery/img4.jpg';
+import img5 from './assets/gallery/img5.jpg';
+import img6 from './assets/gallery/img6.jpg';
+import img7 from './assets/gallery/img7.jpg';
+import img8 from './assets/gallery/img8.jpg';
+import img9 from './assets/gallery/img9.jpg';
+import img10 from './assets/gallery/img10.jpg';
+import img11 from './assets/gallery/img11.jpg';
+import img12 from './assets/gallery/img12.jpg';
+import img13 from './assets/gallery/img13.jpg';
+import img14 from './assets/gallery/img14.jpg';
+import img15 from './assets/gallery/img15.jpg';
+import img16 from './assets/gallery/img16.jpg';
+import img17 from './assets/gallery/img17.jpg';
+import img18 from './assets/gallery/img18.jpg';
+import img19 from './assets/gallery/img19.jpg';
+import img20 from './assets/gallery/img20.jpg';
+import img21 from './assets/gallery/img21.jpg';
+import img22 from './assets/gallery/img22.jpg';
+import img23 from './assets/gallery/img23.jpg';
+import img24 from './assets/gallery/img24.jpg';
+
+export const gallery = [
+    img1,
+    // img2,
+    img3,
+    img4,
+    img5,
+    img6,
+    img7,
+    img8,
+    img9,
+    img10,
+    img11,
+    img12,
+    img13,
+    img14,
+    img15,
+    img16,
+    img17,
+    img18,
+    img19,
+    img20,
+    img21,
+    img22,
+    img23,
+    img24
+]
+
+export default gallery;
--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import vishnu from '../assets/Vishnuyelde.jpeg'
+import gallery from '../gallery';
 
 const Gallery = () => {
   return (
     <div className='container'>
       <h1 className='text-center font-bold text-3xl my-10'>Gallery</h1>
       <div className='grid grid-cols-3 gap-4 mb-20'> 
-        <img className='object-contain border-gray-500 border-2 shadow-md h-[250px] w-[350px]' src={vishnu} alt="img" width={350} height={250} />
-        <img className='object-contain border-gray-500 border-2 shadow-md h-[250px] w-[350px]' src={vishnu} alt="img" width={350} height={250} />
-        <img className='object-contain border-gray-500 border-2 shadow-md h-[250px] w-[350px]' src={vishnu} alt="img" width={350} height={250} />
+      {gallery.map((items) => {
+        return (
+        <img className='object-cover border-gray-200 border-2 shadow-md h-[250px] w-[350px]' src={items} alt="img" width={350} height={250} />
+        );
+      })}
       </div>
     </div>
   )
