64b782dd771085a68ed3d941bf87ac7267c57a25
Aditya Sharma <adisharma4857@gmail.com>
2023-10-26 23:12:20 +0530

    updated

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,36 +1,42 @@
-// import React from 'react'
-
-// const Login = () => {
-//   return (
-//     <div className='container flex flex-col items-center'>
-//         <h1 className='text-2xl font-semibold my-10'>Login</h1>
-//         <form action="">
-//             <div className='flex flex-col gap-2'>
-//                 <label htmlFor="username">Username</label>
-//                 <input type="text" name="username" id="username" className='border-2 border-gray-400' />
-//             </div>
-//             <div className='flex flex-col gap-2'>
-//                 <label htmlFor="username">Password</label>
-//                 <input type="password" name="username" id="username" className='border-2 border-gray-400' />
-//             </div>
-//         </form>
-//     </div>
-//   )
-// }
-
-// export default Login
-import React from 'react';
-import { Link } from 'react-router-dom';
-// import Signup from './Signup';
+import React, {useState} from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Login() {
+
+    const navigate = useNavigate();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
+    const loginUser = async (e) => {
+        e.preventDefault();
+
+        const res = await fetch("/signin", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                email, password
+            })
+        });
+
+        const data = res.json();
+
+        if(res.status === 400 || !data){
+            window.alert("Invalid credentials!");
+        }else{
+            window.alert("Logged in successfully");
+            navigate("/");
+        }
+    }
+
     return (
         <div className="relative flex flex-col justify-center py-10 overflow-hidden">
             <div className="w-full p-6 m-auto bg-white rounded-md shadow-xl lg:max-w-xl">
                 <h1 className="text-3xl font-semibold text-center text-blue-900 uppercase">
                     Sign in
                 </h1>
-                <form className="mt-6">
+                <form className="mt-6" method="POST">
                     <div className="mb-2">
                         <label
                             for="email"
@@ -42,6 +48,8 @@ export default function Login() {
                             type="email"
                             className="block w-full px-4 py-2 mt-2 text-blue-900 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
                             required
+                            onChange={(e)=> setEmail(e.target.value)}
+                            value={email}
                         />
                     </div>
                     <div className="mb-2">
@@ -55,6 +63,8 @@ export default function Login() {
                             type="password"
                             className="block w-full px-4 py-2 mt-2 text-blue-900 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
                             required
+                            onChange={(e)=> setPassword(e.target.value)}
+                            value={password}
                         />
                     </div>
                     <a
@@ -64,7 +74,9 @@ export default function Login() {
                         Forgot Password?
                     </a>
                     <div className="mt-6">
-                        <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-blue-700 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600">
+                        <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-blue-700 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+                        onClick={loginUser}
+                        >
                             Login
                         </button>
                     </div>
